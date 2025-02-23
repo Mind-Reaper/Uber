62619fa345509d925a64e2bb6042dcc14f877d9d
Mind-Reaper <onadipedaniel@gmail.com>
2025-02-23 15:13:12 +0100

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Daniel Onadipe on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
